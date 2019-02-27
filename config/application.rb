f385a7d787b5752ce008da2b5d75e88ba7ff4224
Gustavo Salazar <guga.salazar.loor@gmail.com>
2019-02-27 20:57:50 -0300

    Rack CORS gem to allow cross origin access

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,18 @@ Bundler.require(*Rails.groups)
 
 module Rha
   class Application < Rails::Application
+
+    # Configure Rack CORS
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', 
+          headers: :any, 
+          expose: ['access-token', 'expiry', 'token-type', 'uid', 'client'],
+          methods: [:get, :post, :options, :delete, :put]
+      end
+    end
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
